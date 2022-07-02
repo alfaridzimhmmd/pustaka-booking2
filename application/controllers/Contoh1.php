20679a703c0588ecff04b98d49d6a67e4a512eca
Denjiiest <jimenk123@gmail.com>
2022-07-02 19:23:48 +0700

    memperbaiki komponen yang salah

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,10 +4,10 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h>Perkenalkan</h1>";
-        echo" Nama saya Suryanto Aji Purnomo
-              Saya tinggal di daerah Jakarta Utara
+        echo" Nama saya Muhammad Alfaridzi
+              Saya tinggal di daerah Jakarta Timur
               Olahraga yang saya sukai adalah
-              Bulutangkis";
+              Sepak Bola";
 
     }
 }
--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,13 +2,13 @@
  <h1><?php echo $judul ?></h1>
  <h4>Nama</h4>
  <ul type="disc">
- <li>Nama Depan : Suryanto Aji</li>
- <li>Nama Belakang : Purnomo </li>
+ <li>Nama Depan : Muhammad</li>
+ <li>Nama Belakang : Alfaridzi </li>
  </ul>
  <br>
  <h4>Alamat</h4>
  <ul type="none">
- <li> Jalan Budimulia</li>
+ <li> Jalan Pisangan Lama 2 </li>
  </ul>
  
  <h4>Tempat Lahir</h4>
@@ -18,7 +18,7 @@
  <h4>Olah Raga Favorit</h4>
  <ul type="square">
  <li>Sepakbola</li>
- <li>BuluTangkis</li>
- <li>Berenang</li>
+ <li>Bersepeda</li>
+ <li>Basket</li>
  </ul>
 </section>
